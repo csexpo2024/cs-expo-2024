960703a781abd15c11cc0484685edcc30fcd3539
2wons <013ayu@gmail.com>
2024-10-30 02:56:15 +0800

    chore: add projects data

---
+++ src/constants/projects.ts
@@ -0,0 +1,150 @@
+
+interface Project {
+    title: string;
+    group: string;
+    description: string;
+    image: string;
+}
+
+/* TODO: fill missing data */
+export const projects: Record<string, Project> = {
+    "agila": {
+        title: "AGILA",
+        group: "Agatha",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor, orci non placerat elementum, felis nunc suscipit tortor, vitae vestibulum nulla risus eu sem. Quisque pretium finibus massa, vitae vulputate mi posuere dictum. Ut sed nibh blandit, convallis leo at, euismod dolor. Cras elementum nulla id tempus ultricies. ",
+        image: "",
+       
+    },
+    "meowtion": {
+        title: "MEOWTION CAPTURE",
+        group: "Algoriteam",
+        description: "",
+        image: "",
+    },
+    "mlfds": {
+        title: "MLFDS",
+        group: "Chilimansi",
+        description: "",
+        image: "",
+    },
+    "fit-vision": {
+        title: "FIT VISION",
+        group: "Code",
+        description: "",
+        image: "",
+    },
+    "carident": {
+        title: "CARIDENT AI MEDIX",
+        group: "Code Geass",
+        description: "",
+        image: "",
+    },
+    "tala": {
+        title: "Tala",
+        group: "Codex",
+        description: "",
+        image: "",
+    },
+    "csgo": {
+        title: "CSGO System",
+        group: "Computer Science Global Offensive",
+        description: "",
+        image: "",
+    },
+    "mimical": {
+        title: "Mimical",
+        group: "CtrlAltDelete",
+        description: "",
+        image: "",
+    },
+    "ensys-chat": {
+        title: "Ensys Chat",
+        group: "Ensys",
+        description: "",
+        image: "",
+    },
+    "smarteyes": {
+        title: "SmartEyes",
+        group: "GIGGA",
+        description: "",
+        image: "",
+    },
+    "elixir": {
+        title: "Elixir",
+        group: "HUI",
+        description: "",
+        image: "",
+    },
+    "onics": {
+        title: "Onics",
+        group: "Hurtz",
+        description: "",
+        image: "",
+    },
+    "intelliwatch": {
+        title: "Intelliwatch",
+        group: "Intelliwatch",
+        description: "",
+        image: "",
+    },
+    "book-i": {
+        title: "Book-I",
+        group: "Jathro's Matrix",
+        description: "",
+        image: "",
+    },
+    "marikit": {
+        title: "Marikit",
+        group: "JLNN",
+        description: "",
+        image: "",
+    },
+    "lambda": {
+        title: "Lambda",
+        group: "Lambda",
+        description: "",
+        image: "",
+    },
+    "smart-tasks": {
+        title: "SmartTasks",
+        group: "LMDIFY",
+        description: "",
+        image: "",
+    },
+    "visual-sql": {
+        title: "VisualSQL",
+        group: "LMNTRX",
+        description: "",
+        image: "",
+    },
+    "etbx": {
+        title: "ETBX",
+        group: "Lorem Ipsum",
+        description: "",
+        image: "",
+    },
+    "pnp": {
+        title: "Puno Ng Pagmamahal",
+        group: "PnP",
+        description: "",
+        image: "",
+    },
+    "ultrascan": {
+        title: "UltraScan",
+        group: "Samsan",
+        description: "",
+        image: "",
+    },
+    "smish-x": {
+        title: "Smish X",
+        group: "Strawhats",
+        description: "",
+        image: "",
+    },
+    "theta": {
+        title: "Theta",
+        group: "Theta",
+        description: "",
+        image: "",
+    },
+}
