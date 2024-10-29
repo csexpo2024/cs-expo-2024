bd33a79e1688478c623599e1719d9abf592a2cb0
2wons <013ayu@gmail.com>
2024-10-29 15:56:39 +0800

    feat: add landing page hero

--- src/routes/landing.tsx
+++ src/routes/landing.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@/components/ui/button";
+import LogoSilver from "@/assets/img/logo-silver-cropped.png";
+import BgRed from "@/assets/img/bg/09-red-grid.png";
 
 const Landing = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold font-heading">CS Expo 2024</h1>
-      <Button>CS Expo 2024</Button>
+    <div
+      className="flex flex-col gap-5 justify-center items-center py-16"
+      style={{
+        backgroundImage: `url(${BgRed})`,
+      }}
+    >
+      <img
+        className="text-6xl font-bold font-heading max-w-xs p-5"
+        src={LogoSilver}
+      />
+      <h2 className="text-3xl px-2 md:p=0 font-heading-italic lg:w-1/2 text-center uppercase">
+        Unveiling <span className="text-yellow-500">possibilites</span> and
+        creating new <span className="text-red-500">realities</span>
+      </h2>
     </div>
   );
 };
