53b1ef636d2441bd640e682e0e8013ce030d1ccc
2wons <013ayu@gmail.com>
2024-11-11 21:06:48 +0800

    fix: update lmntrx research title

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -240,7 +240,7 @@ export const projects: Record<string, Project> = {
     },
     "visual-sql": {
         title: "VisualSQL",
-        research_title: "Lorem ipsum",
+        research_title: "Visualsql: Visual Programming In Learning Structured Query Language Using Rule- Based Expert System For Detecting Pessimistic Behavior",
         group: "LMNTRX",
         groupid: "lmntrx",
         description: "This paper presents the development and evaluation of VisualSQL, an Intelligent Tutoring System (ITS) aimed at improving SQL learning through the detection of pessimistic behavior in students. VisualSQL integrates Convolutional Neural Networks (CNNs) for facial expression analysis and a Rule-Based Expert System to provide personalized feedback and assessments. The study employed a descriptive-developmental methodology and followed an iterative Scrum process. The system achieved a 92% detection accuracy, demonstrating its effectiveness in enhancing student engagement and learning outcomes. A dataset of 10,000 facial expression images with 70,000 annotations (7 per image) was used to train the CNN model. Evaluation involved 37 students, 1 psychologist, and 4 IT experts. The Technology Acceptance Model (TAM) was utilized for user acceptance, while expert evaluation, based on ISO-25010 standards, confirmed the system's functional stability, performance efficiency, and usability. Visual SQL’s mean Average Precision (mAP@0.5) across all classes was 0.945. User feedback indicated high perceived usefulness (4.52/5) and ease of use (4.72/5), with experts rating the user interface highly for intuitiveness (4.89/5). The system’s weakest aspect was data encryption strength, rated at 3.89/5.",
