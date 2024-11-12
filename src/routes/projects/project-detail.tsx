d9475b10866f482c07b8174576db497a4a22c1f5
2wons <013ayu@gmail.com>
2024-11-13 01:46:34 +0800

    refactor: improve project-detail page

--- src/routes/projects/project-detail.tsx
+++ src/routes/projects/project-detail.tsx
@@ -2,8 +2,6 @@ import { projects as data } from "@/constants/projects";
 
 import { useParams } from "react-router-dom";
 
-import GroupPhoto from "@/assets/img/group-photo-placeholder.jpg";
-
 import {
   Carousel,
   CarouselContent,
@@ -14,8 +12,11 @@ import {
 
 import { useState } from "react";
 
+import CautionTapeGroup from "@/assets/img/caution-tape-shadowed.png";
+
+import { PolaroidFrame } from "@/components/polaroid-frame";
+
 /* TODO
- * Update for group banner
  * Update for gallery
  */
 const Project = () => {
@@ -27,7 +28,6 @@ const Project = () => {
     "https://preview.redd.it/e54t88z9l2p71.jpg?width=640&crop=smart&auto=webp&s=471982e45741e3afeb0e03c4c48166b092ce1494",
     "https://preview.redd.it/ybi78y1al2p71.jpg?width=640&crop=smart&auto=webp&s=5fd43ab51ad7b71a084cdaf9971e79144bf557bf",
     "https://dbkpop.com/wp-content/uploads/2021/09/aespa_savage_teaser_hallucination_quest_1_ningning_1.jpg",
-    "https://static.wikia.nocookie.net/aespa/images/4/4b/Aespa_Whiplash_Concept_Photo_3.jpg",
   ];
 
   const project = projectid ? data[projectid.toLowerCase()] : null;
@@ -45,87 +45,110 @@ const Project = () => {
   }
 
   return (
-    <div className="px-4 xl:px-40">
-      <div
-        className="group-backdrop right-0 sm:mx-28 absolute z-[-100] block max-w-full h-[500px] top-0 left-0 bg-center bg-no-repeat bg-cover animate-fade-in duration-700"
-        style={{
-          backgroundImage: `url(${GroupPhoto})`,
-        }}
-      ></div>
-      <img
-        rel="preload"
-        src={GroupPhoto}
-        className="h-[250px] sm:h-[350px] opacity-0 bg-transparent"
-        alt=""
-      />
-      <div className="sm:grid grid-cols-4 sm:gap-8">
-        {/* Poster Column */}
-        <div className="col-span-1 place-items-center">
-          <div className="sm:flex flex-col mx-2 sm:mx-1 sticky top-0 pt-2 pb-3 float-right w-1/4 sm:w-full sm:float-none">
-            <div className="drop-shadow-2xl">
-              <img
-                src={`/img/posters/${project.groupid}-poster.jpg`}
-                alt=""
-                className="rounded-lg w-full drop-shadow-[0_35px_35px_rgba(0,0,0,0.55)] border-[1px] border-gray-50 border-opacity-15"
-              />
-            </div>
-          </div>
-        </div>
-        {/* Project info Column */}
-        <div className="sm:col-span-3 px-2">
-          {/* Header */}
-          <div className="flex flex-wrap pb-4 space-y-2 gap-2 items-end [text-shadow:_0_4px_5px_rgba(0,0,0,0.95)]">
-            <h1 className="text-4xl lg:text-5xl font-heading ">
-              {project?.title}
-            </h1>
-            <div className="flex flex-wrap space-x-2 text-lg p-2 [text-shadow:_0_4px_200px_rgba(0,0,0,0.95)]">
-              <p className="text-muted-foreground">by</p>
-              <p className="text-yellow-500 underline">{project?.group}</p>
-            </div>
-          </div>
-          <h2 className="text-lg font-normal pb-2">
-            {project?.research_title}
-          </h2>
-          {/* More Info */}
-          <div className="lg:grid grid-cols-5 gap-5 py-5">
-            <div className="col-span-3">
-              <p
-                className={`font-mono text-sm sm:text-base text-muted-foreground ${
-                  !showMore && "line-clamp-6 sm:line-clamp-[15]"
-                } lg:max-w-xl`}
-              >
-                {project?.description}
-              </p>
-              <span
-                onClick={() => setShowMore(!showMore)}
-                className="text-sky-500 font-semibold cursor-pointer"
-              >
-                {showMore ? "Show less" : "Show more"}
-              </span>
-            </div>
-            <div className="col-span-2 py-5 lg:py-0">
-              <div className="flex flex-col gap-5 bg-white bg-opacity-15 backdrop-blur-md rounded w-full p-5 sm:w-auto min-w-min">
-                <GroupTags label="Members" tags={members!} />
-                <GroupTags label="Mentor" tags={mentor!} />
-                <GroupTags label="Concepts" tags={concepts!} />
+    <div className="">
+      <div className="px-4 xl:px-52">
+        <div
+          className="group-backdrop sm:mx-28 absolute z-[0] max-w-full h-[500px] right-0 top-0 left-0 bg-center bg-no-repeat bg-cover animate-fade-in duration-700"
+          style={{
+            backgroundImage: `url(/img/team/${project?.groupid}.jpg)`,
+          }}
+        ></div>
+        <img
+          rel="preload"
+          src={`/img/team/${project?.groupid}.jpg`}
+          className="h-[250px] sm:h-[350px] opacity-0 bg-opacity-0 bg-transparent"
+          alt=""
+        />
+        <div className="sm:grid grid-cols-4 sm:gap-8 relative z-10">
+          {/* Poster Column */}
+          <div className="col-span-1 place-items-center">
+            <div className="sm:flex flex-col mx-2 sm:mx-1 sticky top-0 pt-2 pb-3 float-right w-1/4 sm:w-full sm:float-none">
+              <div className="drop-shadow-2xl">
+                <img
+                  src={`/img/posters/${project.groupid}-poster.jpg`}
+                  alt=""
+                  className="rounded-lg w-full drop-shadow-[0_35px_35px_rgba(0,0,0,0.55)] border-[1px] border-gray-50 border-opacity-15"
+                />
               </div>
             </div>
           </div>
-          <h3 className="font-heading text-lg py-8">AVP</h3>
-          <div className="relative overflow-hidden w-full pt-[56.25%]">
-            <div className="absolute top-0 left-0 bottom-0 right-0 w-full h-full">
-              <Avp
-                link={
-                  "https://www.youtube.com/embed/jQNEiHzIcX4?si=z_OGbedZhXvKgCNR"
-                }
-              />
+          {/* Project info Column */}
+          <div className="sm:col-span-3 px-2 z-[100]">
+            {/* Header */}
+            <div className="flex flex-wrap pb-4 space-y-2 items-end [text-shadow:_0_4px_5px_rgba(0,0,0,0.95)]">
+              <h1 className="text-3xl lg:text-5xl font-heading ">
+                {project?.title}
+              </h1>
+              <div className="flex flex-wrap space-x-2 text-lg pl-2 p-2 [text-shadow:_0_4px_200px_rgba(0,0,0,0.95)]">
+                <p>
+                  <span className="text-muted-foreground">by</span>{" "}
+                  <span className="text-yellow-500 underline">
+                    {project?.group}
+                  </span>
+                </p>
+              </div>
+            </div>
+            <h2 className="font-medium pb-2">{project?.research_title}</h2>
+            {/* More Info */}
+            <div className="lg:grid grid-cols-5 gap-5 py-5">
+              <div className="col-span-3">
+                <p
+                  className={`font-mono text-sm sm:text-base text-muted-foreground ${
+                    !showMore && "line-clamp-6 sm:line-clamp-[15]"
+                  }`}
+                >
+                  {project?.description}
+                </p>
+                <span
+                  onClick={() => setShowMore(!showMore)}
+                  className="text-sky-500 font-semibold cursor-pointer"
+                >
+                  {showMore ? "Show less" : "Show more"}
+                </span>
+              </div>
+              {/* Tag Groups */}
+              <div className="col-span-2 py-5 lg:py-0">
+                <div className="flex flex-col gap-5 bg-white bg-opacity-15 backdrop-blur-md rounded w-full p-5 sm:w-auto min-w-min">
+                  <GroupTags label="Members" tags={members!} />
+                  <GroupTags label="Mentor" tags={mentor!} />
+                  <GroupTags label="Concepts" tags={concepts!} />
+                </div>
+              </div>
+            </div>
+            <h3 className="font-heading text-lg py-8">
+              AVP{" "}
+              <span>
+                <hr />
+              </span>
+            </h3>
+            <div className="relative overflow-hidden w-full pt-[56.25%]">
+              <div className="absolute top-0 left-0 bottom-0 right-0 w-full h-full">
+                <Avp
+                  link={
+                    "https://www.youtube.com/embed/jQNEiHzIcX4?si=z_OGbedZhXvKgCNR"
+                  }
+                />
+              </div>
+            </div>
+            <h3 className="font-heading text-lg py-8">
+              Gallery{" "}
+              <span>
+                <hr />
+              </span>
+            </h3>
+            <div className="w-full px-10">
+              <Gallery images={gallery} />
             </div>
           </div>
-          <h3 className="font-heading text-lg py-8">Gallery</h3>
-          <div className="max-w-4xl px-10">
-            <Gallery images={gallery} />
-          </div>
         </div>
+      </div>
+      {/* Caution Tape Footer */}
+      <div className="h-56 overflow-hidden">
+        <img
+          src={CautionTapeGroup}
+          alt="Caution Tape"
+          className="w-full h-auto"
+        />
       </div>
     </div>
   );
@@ -134,7 +157,7 @@ const Project = () => {
 const GroupTags = ({ label, tags }: { label: string; tags: string[] }) => {
   return (
     <div>
-      <p className="text-muted-foreground">{label}</p>
+      <p className="text-muted-foreground pb-1">{label}</p>
       <div className="flex flex-wrap gap-2 items-center">
         {tags.map((tag, i) => (
           <span key={i} className="bg-neutral-900 p-2 text-sm rounded-md">
@@ -164,14 +187,21 @@ const Gallery = ({ images }: { images: string[] }) => {
   return (
     <Carousel
       opts={{
-        align: "center",
         loop: true,
       }}
     >
       <CarouselContent>
         {images.map((image, i) => (
           <CarouselItem key={i}>
-            <img src={image} alt="" className="w-full h-96 object-contain" />
+            <div className="flex justify-center items-center">
+              <PolaroidFrame size="lg" nonPadded>
+                <img
+                  src={image}
+                  alt=""
+                  className="w-full h-96 object-contain"
+                />
+              </PolaroidFrame>
+            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
